e21a975866e9d1296250e5216fa94af5b1b580fb
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-03-15 23:44:26 +0530

    intial-1

--- .next/dev/static/chunks/components_Map_tsx_85d04867._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_b3d7cc77._.js",
-  "static/chunks/components_Map_tsx_d5f8fd9f._.js"
-],
-    source: "dynamic"
-});
